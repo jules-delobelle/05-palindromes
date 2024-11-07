696a4d9dac5f712757e612e9c9f49efcf6625e4a
Jules <61208950+jules-delobelle@users.noreply.github.com>
2024-11-07 09:38:37 +0000

    coded palindrome

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+"Palindrome program"
+
+import string
+from unidecode import unidecode
+
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """
+    Check if a string p is a palindrome
+    """
+    p = unidecode(p.replace(" ", "")).lower()
+    p = p.translate(str.maketrans("", "", string.punctuation))
 
-    # votre code ici
-    
-    return False
+    return p == p[::-1]
 
 #### Fonction principale
 
 
 def main():
 
+    """
+    main function
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
